c288a5058dbf932dc6bce738ac9b0bea9adadfbc
Adam Cowdy <cowdyadam@gmail.com>
2017-10-28 18:41:19 +0100

    Add stub methods for game

--- longboi/src/game.ts
+++ longboi/src/game.ts
@@ -1,8 +1,25 @@
 export namespace game {
-    let canvas = <HTMLCanvasElement> document.getElementById("longboi-canvas")
-    let context = canvas.getContext("2d")
+    const canvas = <HTMLCanvasElement> document.getElementById("longboi-canvas")
+    const context = canvas.getContext("2d")
 
-    export function start() {
-        // TODO
+    export function run() {
+        let isRunning = true;
+        start()
+        while (isRunning) {
+            draw()
+            update()
+        }
+    }
+
+    function start() {
+        // TODO: create snek
+    }
+
+    function draw() {
+        // TODO: draw level, then the food, then snek
+    }
+
+    function update() {
+        // TODO: move snek, deal with food behaviour
     }
 }
