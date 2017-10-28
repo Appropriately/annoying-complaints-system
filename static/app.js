1c3c4d4d33f35f1aeeb2a2b47b32eeccec73adbd
Sean Lewis <sean.alan.lewis@gmail.com>
2017-10-28 22:41:59 +0100

    Authenticated login

--- static/app.js
+++ static/app.js
@@ -1,23 +1,27 @@
-  var app = angular.module('Complaints', [])
+  var app = angular.module('Complaints', []);
 
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  app.controller('MainCtrl', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
     $scope.currentView = "welcome";
     $scope.firstName = ""; $scope.lastName = "";
     $scope.email = ""; $scope.dob = "";
     $scope.complaint = "";
+    
+    $scope.formData = {};
 
     $scope.navigate = function(page) {
       $scope.currentView = page;
     }; // navigate
 
     $scope.authenticateUser = function(username, password) {
-      var authenticate = $http ({
-        method : "post",
+      $http({
+        method: "POST",
         url: "/api/authenticateUser",
         data: {
           username: username,
           password: password
         }
+      }).success(function(data) {
+        if (data === "authenticated") $scope.navigate('introduction');
       });
     };
   }])
