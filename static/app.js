66fba888b474c8bb13a9601c20eb7bd343239bda
Sean Lewis <sean.alan.lewis@gmail.com>
2017-10-28 21:02:23 +0100

    I stashed things oops

--- static/app.js
+++ static/app.js
@@ -1,11 +1,23 @@
   var app = angular.module('Complaints', [])
-  .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.currentView = "introduction";
-        $scope.firstName = ""; $scope.lastName = "";
-        $scope.email = ""; $scope.dob = "";
-        $scope.complaint = "";
 
-        $scope.navigate = function(page) {
-          $scope.currentView = page;
-        }; // navigate
-  }]);
+  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.currentView = "welcome";
+    $scope.firstName = ""; $scope.lastName = "";
+    $scope.email = ""; $scope.dob = "";
+    $scope.complaint = "";
+
+    $scope.navigate = function(page) {
+      $scope.currentView = page;
+    }; // navigate
+
+    $scope.authenticateUser = function(username, password) {
+      var authenticate = $http ({
+        method : "post",
+        url: "/api/authenticateUser",
+        data: {
+          username: username,
+          password: password
+        }
+      });
+    };
+  }])
