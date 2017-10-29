2bbdecc149940517247588e59a42c25d840e6dd6
Joe <josephbroseph7@gmail.com>
2017-10-29 03:52:04 +0000

    basic profiling system in place

--- textAnalysis.py
+++ textAnalysis.py
@@ -1,17 +1,39 @@
 from textblob import TextBlob
 from nltk.corpus import wordnet as wn, wordnet
 
-
 def makePositive(originalComplaint):
+    global wordCount
+    global adjectiveWordCount
+    global nounWordCount
+    global wordChanges      # technically bad words detected
+
+    adjectiveWordCount = 0
+    nounWordCount = 0
+    wordChanges=0
+
     textBlob = TextBlob(originalComplaint)
+    wordCount = len(textBlob.words);
     acceptedTags = ["JJ", "JJR", "JJS", "VBP", "VB", "VBD", "VBN", "VBG", "VBZ"]
+    adjTags = ["JJ", "JJR", "JJS"]
+    verbTags = ["VBP", "VB", "VBD", "VBN", "VBG", "VBZ"]
+
+
     for index in range(0, len(textBlob.tags)):
         word = textBlob.words[index]
+
+        if(textBlob.tags[index][1] in adjTags):
+            adjectiveWordCount += 1
+
+        if(textBlob.tags[index][1] in verbTags):
+            nounWordCount += 1
+
         if(textBlob.tags[index][1] in acceptedTags):
             wordBlob = TextBlob(textBlob.words[index])
             sentiment = wordBlob.sentiment[0]
             if(wordBlob.sentiment[0] < 0):
+                wordChanges += 1
                 textBlob = textBlob.replace(textBlob.tags[index][0], getAntonym(textBlob.tags[index][0]))
+    #return str(makeUserProfile())
     return str(textBlob)
 
 def getAntonym(word):
@@ -23,3 +45,24 @@ def getAntonym(word):
 
 def correctSpelling(originalComplaint):
     textBlob = TextBlob(originalComplaint)
+
+def makeUserProfile():
+    # return dictionary of user profiling summary
+    profile = {"Full Name":"Nice FANCY surname you have"}
+
+    if(adjectiveWordCount > 1):
+        profile['smart aleck'] = "You heard. "
+
+    if(nounWordCount > 1):
+        profile['introvert'] = "Yeah you should probably get out more... Stop writing complaints whilst you're at it. "
+
+    if (wordCount > 30):
+        profile['bitter'] = "Jeeeez, someone got out of the wrong side of the bed this morning. "
+    elif (wordCount > 10):
+        profile['bitter'] = "Awwww is someone feeling a bit bitter... Maybe you should stop complaining all the time. "
+
+    if(wordChanges > 5):
+        profile['pessimist'] = "This is a different level of pessimism now. At this point you're probably beyond help. "
+    elif (wordChanges > 0):
+        profile['pessimist'] = "You're a pessimist. You gotta lighten up, think more about the good things. The glass is half full.. "
+    return profile
